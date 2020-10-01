4de4c71cbf062473f6b032b3f2d343a896e9fff1
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-01 23:28:59 +0600

    A simple rest controller

---
+++ src/main/java/com/example/RESTfulAPISpringBoot/BookController.java
@@ -0,0 +1,15 @@
+package com.example.RESTfulAPISpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+    @GetMapping("/books")
+    public List<Book> getAll(){
+        return Arrays.asList(new Book(1, "test", "test"));
+    }
+}
