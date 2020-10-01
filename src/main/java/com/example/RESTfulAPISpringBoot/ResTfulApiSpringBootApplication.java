4c19f5d6297bb0892685f263c425e1b96c2029b1
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-01 22:33:28 +0600

    Spring boot starter project

---
+++ src/main/java/com/example/RESTfulAPISpringBoot/ResTfulApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulAPISpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTfulAPISpringBoot/ResTfulApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulAPISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
