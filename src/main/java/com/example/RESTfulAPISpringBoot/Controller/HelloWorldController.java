170d71eb258da1c11ae00abd9a28e63119d04d82
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-04 00:53:55 +0600

    use path variable

--- src/main/java/com/example/RESTfulAPISpringBoot/Controller/HelloWorldController.java
+++ src/main/java/com/example/RESTfulAPISpringBoot/Controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.RESTfulAPISpringBoot.Controller;
 
 import com.example.RESTfulAPISpringBoot.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello world";
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+
+    public String helloWorldPath(@PathVariable String name){
+        return "Hello world " + name;
     }
 
     @GetMapping(path = "/hello-world-bean")
