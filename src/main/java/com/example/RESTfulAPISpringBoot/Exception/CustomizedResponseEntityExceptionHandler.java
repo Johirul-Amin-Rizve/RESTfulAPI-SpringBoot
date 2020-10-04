1f45d58781604fdbfe60a088cf4025ecf94d2c34
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-04 15:08:07 +0600

    add validation

--- src/main/java/com/example/RESTfulAPISpringBoot/Exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/RESTfulAPISpringBoot/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.example.RESTfulAPISpringBoot.Exception;
 
-import org.aspectj.weaver.ast.Not;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.Date;
 
 @ControllerAdvice
@@ -31,5 +31,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
 
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid
+            (MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), "Validation Failed");
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
